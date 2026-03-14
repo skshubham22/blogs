0cf1a4e98b22e69a04ef7be2e5541e600fa79e47
User <user@example.com>
2026-03-14 18:49:48 +0530

    Fix Render build failure by adding default storage configuration

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -135,6 +135,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
